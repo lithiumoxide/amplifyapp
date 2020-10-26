24ec9fd04d606c4b413655dd94e8962d44e225fe
Conor Farrell <conor.farrell8@gmail.com>
2020-10-26 17:30:29 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           <h1>Hello from V2</h1>
-        </a>
       </header>
     </div>
   );
